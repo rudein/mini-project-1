5ec37585626b0da829943e9da63101e7be602c6a
rudein <115561871+rudein@users.noreply.github.com>
2022-10-13 18:56:08 +0530

    1

--- grading/database/grading_db.sql
+++ grading/database/grading_db.sql
@@ -1,33 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 14, 2020 at 06:25 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `grading_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `advisers`
---
-
 CREATE TABLE `advisers` (
   `adviser_id` int(10) NOT NULL,
   `name` varchar(50) NOT NULL,
@@ -37,12 +12,6 @@ CREATE TABLE `advisers` (
   `school_year` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `attendance`
---
-
 CREATE TABLE `attendance` (
   `ATT_ID` int(30) NOT NULL,
   `STUDENT_ID` int(30) NOT NULL,
@@ -51,10 +20,6 @@ CREATE TABLE `attendance` (
   `DAYS_OF_CLASSES` int(5) NOT NULL,
   `DAYS_PRESENT` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `attendance`
---
 
 INSERT INTO `attendance` (`ATT_ID`, `STUDENT_ID`, `SYI_ID`, `MONTH`, `DAYS_OF_CLASSES`, `DAYS_PRESENT`) VALUES
 (1, 1, 1, 'June', 10, 10),
@@ -82,22 +47,10 @@ INSERT INTO `attendance` (`ATT_ID`, `STUDENT_ID`, `SYI_ID`, `MONTH`, `DAYS_OF_CL
 (23, 1, 2, 'April', 0, 0),
 (24, 1, 2, 'May', 0, 0);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `awss`
---
-
 CREATE TABLE `awss` (
   `id` int(200) NOT NULL,
   `ff` int(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `data_b`
---
 
 CREATE TABLE `data_b` (
   `db_id` int(10) NOT NULL,
@@ -105,23 +58,11 @@ CREATE TABLE `data_b` (
   `date_added` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `data_base`
---
-
 CREATE TABLE `data_base` (
   `db_id` int(10) NOT NULL,
   `dn_name` varchar(50) NOT NULL,
   `date_added` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `grade`
---
 
 CREATE TABLE `grade` (
   `grade_id` int(10) NOT NULL,
@@ -129,21 +70,11 @@ CREATE TABLE `grade` (
   `status` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `grade`
---
-
 INSERT INTO `grade` (`grade_id`, `grade`, `status`) VALUES
 (1, '7', ''),
 (2, '8', ''),
 (3, '9', ''),
 (4, '10', '');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `grades_per_subject`
---
 
 CREATE TABLE `grades_per_subject` (
   `ID` int(11) NOT NULL,
@@ -154,11 +85,6 @@ CREATE TABLE `grades_per_subject` (
   `GRADES` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `history_log`
---
 
 CREATE TABLE `history_log` (
   `log_id` int(10) NOT NULL,
@@ -167,9 +93,6 @@ CREATE TABLE `history_log` (
   `date_added` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `history_log`
---
 
 INSERT INTO `history_log` (`log_id`, `transaction`, `user_id`, `date_added`) VALUES
 (1, 'logged out', 4, '2020-10-14 23:58:21'),
@@ -194,30 +117,14 @@ INSERT INTO `history_log` (`log_id`, `transaction`, `user_id`, `date_added`) VAL
 (20, 'logged out', 1, '2020-10-15 00:17:55'),
 (21, 'logged in', 2, '2020-10-15 00:18:03');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `program`
---
-
 CREATE TABLE `program` (
   `PROGRAM_ID` int(20) NOT NULL,
   `PROGRAM` varchar(10) NOT NULL,
   `DESCRIPTION` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `program`
---
-
 INSERT INTO `program` (`PROGRAM_ID`, `PROGRAM`, `DESCRIPTION`) VALUES
 (1, 'Regular', 'Regular');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `promotion_candidates`
---
 
 CREATE TABLE `promotion_candidates` (
   `id` int(10) NOT NULL,
@@ -225,18 +132,8 @@ CREATE TABLE `promotion_candidates` (
   `SY` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `promotion_candidates`
---
-
 INSERT INTO `promotion_candidates` (`id`, `STUDENT_ID`, `SY`) VALUES
 (1, 1, '2016-2017');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `school_year`
---
 
 CREATE TABLE `school_year` (
   `SY_ID` int(10) NOT NULL,
@@ -244,21 +141,11 @@ CREATE TABLE `school_year` (
   `status` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `school_year`
---
-
 INSERT INTO `school_year` (`SY_ID`, `school_year`, `status`) VALUES
 (1, '2016-2017', 'No'),
 (2, '2017-2018', 'Yes'),
 (3, '2018-2019', 'No'),
 (4, '2019-2020', 'No');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `students`
---
 
 CREATE TABLE `students` (
   `STUDENT_ID` int(50) NOT NULL,
@@ -282,12 +169,6 @@ CREATE TABLE `students` (
   `BIRTH_PLACE` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `student_info`
---
-
 CREATE TABLE `student_info` (
   `STUDENT_ID` int(50) NOT NULL,
   `LRN_NO` varchar(15) NOT NULL,
@@ -307,18 +188,8 @@ CREATE TABLE `student_info` (
   `PROGRAM` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `student_info`
---
-
 INSERT INTO `student_info` (`STUDENT_ID`, `LRN_NO`, `LASTNAME`, `FIRSTNAME`, `MIDDLENAME`, `GENDER`, `DATE_OF_BIRTH`, `ADDRESS`, `BIRTH_PLACE`, `PARENT_GUARDIAN`, `P_ADDRESS`, `INT_COURSE_COMP`, `SCHOOL_YEAR`, `GEN_AVE`, `TOTAL_NO_OF_YEARS`, `PROGRAM`) VALUES
 (1, '12345648', 'Smith', 'John', 'C', 'MALE', '1999-06-23', 'Sample', 'My Town', 'My guardian', 'Sample', 'Sample', '2015-2016', '95', '6', '1');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `student_int_info`
---
 
 CREATE TABLE `student_int_info` (
   `ID` int(30) NOT NULL,
@@ -329,12 +200,6 @@ CREATE TABLE `student_int_info` (
   `GEN_AVE` int(5) NOT NULL,
   `TOTAL_NO_YEARS` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `student_year_info`
---
 
 CREATE TABLE `student_year_info` (
   `SYI_ID` int(11) NOT NULL,
@@ -355,20 +220,9 @@ CREATE TABLE `student_year_info` (
   `ACTION` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `student_year_info`
---
-
 INSERT INTO `student_year_info` (`SYI_ID`, `STUDENT_ID`, `SCHOOL`, `YEAR`, `SECTION`, `TOTAL_NO_OF_YEAR`, `SCHOOL_YEAR`, `ADVANCE_UNIT`, `LACK_UNIT`, `ADVISER`, `GEN_AVE`, `RANK`, `TO_BE_CLASSIFIED`, `TDAYS_OF_CLASSES`, `TDAYS_PRESENT`, `ACTION`) VALUES
 (1, 1, 'School', '1', 'A', 7, '2016-2017', '', '', '', '90', '', 'Grade 8', 120, 120, 'Promoted'),
 (2, 1, 'School', '2', 'a', 8, '2017-2018', '', '', '', '88.125', '', '', 0, 0, 'Promoted');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `subjects`
---
-
 CREATE TABLE `subjects` (
   `SUBJECT_ID` int(11) NOT NULL,
   `SUBJECT` varchar(50) NOT NULL,
@@ -376,19 +230,10 @@ CREATE TABLE `subjects` (
   `DESCRIPTION` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `subjects`
---
 
 INSERT INTO `subjects` (`SUBJECT_ID`, `SUBJECT`, `FOR`, `DESCRIPTION`) VALUES
 (1, 'Englis', 'All', 'English'),
 (2, 'Math', 'All', 'Math');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `total_grades_subjects`
---
 
 CREATE TABLE `total_grades_subjects` (
   `TGS_ID` int(30) NOT NULL,
@@ -404,21 +249,11 @@ CREATE TABLE `total_grades_subjects` (
   `PASSED_FAILED` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `total_grades_subjects`
---
-
 INSERT INTO `total_grades_subjects` (`TGS_ID`, `STUDENT_ID`, `SYI_ID`, `SUBJECT`, `1ST_GRADING`, `2ND_GRADING`, `3RD_GRADING`, `4TH_GRADING`, `UNITS`, `FINAL_GRADES`, `PASSED_FAILED`) VALUES
 (1, 1, 1, 1, '90', '90', '90', '90', '1', '90.00', 'PASSED'),
 (2, 1, 1, 2, '90', '90', '90', '90', '1', '90.00', 'PASSED'),
 (3, 1, 2, 1, '90', '90', '90', '90', '1', '90.00', 'PASSED'),
 (4, 1, 2, 2, '85', '88', '87', '85', '1', '86.25', 'PASSED');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
 
 CREATE TABLE `user` (
   `USER_ID` int(11) NOT NULL,
@@ -429,227 +264,126 @@ CREATE TABLE `user` (
   `USER_TYPE` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `user`
---
 
 INSERT INTO `user` (`USER_ID`, `LASTNAME`, `FIRSTNAME`, `USER`, `PASSWORD`, `USER_TYPE`) VALUES
 (1, 'admin', 'admin', 'admin', '0192023a7bbd73250516f069df18b500', 'ADMINISTRATOR'),
 (2, 'staff', 'staff', 'staff', 'de9bf5643eabf80f4a56fda3bbb84483', 'STAFF');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `advisers`
---
 ALTER TABLE `advisers`
   ADD PRIMARY KEY (`adviser_id`);
 
---
--- Indexes for table `attendance`
---
+
 ALTER TABLE `attendance`
   ADD PRIMARY KEY (`ATT_ID`);
 
---
--- Indexes for table `data_b`
---
+
 ALTER TABLE `data_b`
   ADD PRIMARY KEY (`db_id`);
 
---
--- Indexes for table `data_base`
---
 ALTER TABLE `data_base`
   ADD PRIMARY KEY (`db_id`);
 
---
--- Indexes for table `grade`
---
 ALTER TABLE `grade`
   ADD PRIMARY KEY (`grade_id`);
 
---
--- Indexes for table `grades_per_subject`
---
 ALTER TABLE `grades_per_subject`
   ADD PRIMARY KEY (`ID`);
 
---
--- Indexes for table `history_log`
---
 ALTER TABLE `history_log`
   ADD PRIMARY KEY (`log_id`);
 
---
--- Indexes for table `program`
---
+
 ALTER TABLE `program`
   ADD PRIMARY KEY (`PROGRAM_ID`);
 
---
--- Indexes for table `promotion_candidates`
---
 ALTER TABLE `promotion_candidates`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `school_year`
---
+
 ALTER TABLE `school_year`
   ADD PRIMARY KEY (`SY_ID`);
 
---
--- Indexes for table `students`
---
 ALTER TABLE `students`
   ADD PRIMARY KEY (`STUDENT_ID`);
 
---
--- Indexes for table `student_info`
---
+
 ALTER TABLE `student_info`
   ADD PRIMARY KEY (`STUDENT_ID`);
 
---
--- Indexes for table `student_int_info`
---
 ALTER TABLE `student_int_info`
   ADD PRIMARY KEY (`ID`);
 
---
--- Indexes for table `student_year_info`
---
 ALTER TABLE `student_year_info`
   ADD PRIMARY KEY (`SYI_ID`);
 
---
--- Indexes for table `subjects`
---
 ALTER TABLE `subjects`
   ADD PRIMARY KEY (`SUBJECT_ID`);
 
---
--- Indexes for table `total_grades_subjects`
---
+
 ALTER TABLE `total_grades_subjects`
   ADD PRIMARY KEY (`TGS_ID`);
 
---
--- Indexes for table `user`
---
+
 ALTER TABLE `user`
   ADD PRIMARY KEY (`USER_ID`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `advisers`
---
 ALTER TABLE `advisers`
   MODIFY `adviser_id` int(10) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `attendance`
---
 ALTER TABLE `attendance`
   MODIFY `ATT_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
---
--- AUTO_INCREMENT for table `data_b`
---
+
 ALTER TABLE `data_b`
   MODIFY `db_id` int(10) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `data_base`
---
+
 ALTER TABLE `data_base`
   MODIFY `db_id` int(10) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `grade`
---
+
 ALTER TABLE `grade`
   MODIFY `grade_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- AUTO_INCREMENT for table `grades_per_subject`
---
 ALTER TABLE `grades_per_subject`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `history_log`
---
 ALTER TABLE `history_log`
   MODIFY `log_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
---
--- AUTO_INCREMENT for table `program`
---
+
 ALTER TABLE `program`
   MODIFY `PROGRAM_ID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `promotion_candidates`
---
 ALTER TABLE `promotion_candidates`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `school_year`
---
+
 ALTER TABLE `school_year`
   MODIFY `SY_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- AUTO_INCREMENT for table `students`
---
 ALTER TABLE `students`
   MODIFY `STUDENT_ID` int(50) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `student_info`
---
+
 ALTER TABLE `student_info`
   MODIFY `STUDENT_ID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `student_int_info`
---
+
 ALTER TABLE `student_int_info`
   MODIFY `ID` int(30) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `student_year_info`
---
 ALTER TABLE `student_year_info`
   MODIFY `SYI_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `subjects`
---
 ALTER TABLE `subjects`
   MODIFY `SUBJECT_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `total_grades_subjects`
---
+
 ALTER TABLE `total_grades_subjects`
   MODIFY `TGS_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- AUTO_INCREMENT for table `user`
---
+
 ALTER TABLE `user`
   MODIFY `USER_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
